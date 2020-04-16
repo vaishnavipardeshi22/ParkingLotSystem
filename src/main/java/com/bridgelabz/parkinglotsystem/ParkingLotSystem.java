6c457f2fe4e41faca91f52651bca659a68103c35
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-16 10:48:17 +0530

    ADDED : Function for is vehicle park

--- src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.parkinglotsystem;
 
 public class ParkingLotSystem {
-    public static void main(String[] args) {
-        System.out.println("******************** WELCOME TO PARKING LOT SYSTEM ********************");
+    private Object vehicle;
+
+    public boolean isPark(Object vehicle) {
+        this.vehicle = vehicle;
+        return true;
     }
 }
