9436306ed7bc7e9f762adda8ec44bf627524ca07
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-16 10:47:34 +0530

    ADDED : Test method to check is vehicle park or not

--- src/test/java/com/bridgelabz/parkinglotsystemtest/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/parkinglotsystemtest/ParkingLotSystemTest.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.parkinglotsystemtest;
 
 import com.bridgelabz.parkinglotsystem.ParkingLotSystem;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class ParkingLotSystemTest {
     ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+
+    @Test
+    public void givenParkingLot_WhenVehicleIsParked_ThenReturnTrue() {
+        boolean vehicleIsPark = parkingLotSystem.isPark(new Object());
+        Assert.assertTrue(vehicleIsPark);
+    }
 }
