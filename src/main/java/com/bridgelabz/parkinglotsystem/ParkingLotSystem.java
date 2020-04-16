c7612a00d0de9b463727eabaa096f35487a9581b
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-16 10:33:22 +0530

    ADDED : Welcome message

---
+++ src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.parkinglotsystem;
+
+public class ParkingLotSystem {
+    public static void main(String[] args) {
+        System.out.println("******************** WELCOME TO PARKING LOT SYSTEM ********************");
+    }
+}
---
+++ src/test/java/com/bridgelabz/parkinglotsystemtest/ParkingLotSystemTest.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.parkinglotsystemtest;
+
+import com.bridgelabz.parkinglotsystem.ParkingLotSystem;
+
+public class ParkingLotSystemTest {
+    ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+}
