2821e85529508256bc84c59db58ab9450f527c70
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-16 11:10:15 +0530

    ADDED : Function to unpark vehicle

--- src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
@@ -7,4 +7,12 @@ public class ParkingLotSystem {
         this.vehicle = vehicle;
         return true;
     }
+
+    public boolean isUnPark(Object vehicle) {
+        if (this.vehicle != null && this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
+    }
 }
