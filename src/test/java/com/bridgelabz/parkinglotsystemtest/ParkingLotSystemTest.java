8b1c0a24dffbdf5ef9fe406c7e4caabfeab09f3a
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-16 11:09:32 +0530

    ADDED : Test method for unparked the parked and unparked vehicle

--- src/test/java/com/bridgelabz/parkinglotsystemtest/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/parkinglotsystemtest/ParkingLotSystemTest.java
@@ -2,14 +2,42 @@ package com.bridgelabz.parkinglotsystemtest;
 
 import com.bridgelabz.parkinglotsystem.ParkingLotSystem;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotSystemTest {
-    ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+    ParkingLotSystem parkingLotSystem;
+    Object vehicle;
+
+    @Before
+    public void setUp() throws Exception {
+        parkingLotSystem = new ParkingLotSystem();
+        vehicle = new Object();
+    }
 
     @Test
     public void givenParkingLot_WhenVehicleIsParked_ThenReturnTrue() {
         boolean vehicleIsPark = parkingLotSystem.isPark(new Object());
         Assert.assertTrue(vehicleIsPark);
+    }
+
+    @Test
+    public void givenParkingLot_WhenParkedVehicleIsUnparked_ThenReturnTrue() {
+        parkingLotSystem.isPark(vehicle);
+        boolean isVehiclePark = parkingLotSystem.isUnPark(vehicle);
+        Assert.assertTrue(isVehiclePark);
+    }
+
+    @Test
+    public void givenParkingLot_WhenVehicleIsNotParked_ShouldReturnFalse() {
+        boolean isVehicleParked = parkingLotSystem.isUnPark(vehicle);
+        Assert.assertFalse(isVehicleParked);
+    }
+
+    @Test
+    public void givenParkingLot_WhenUnParkedVehicleWhichIsNotParked_ThenReturnFalse() {
+        parkingLotSystem.isPark(vehicle);
+        boolean isVehicleParked = parkingLotSystem.isUnPark(new Object());
+        Assert.assertFalse(isVehicleParked);
     }
 }
