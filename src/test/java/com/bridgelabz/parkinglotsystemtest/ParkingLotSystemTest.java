ffd96be4537e2f121c4d4d37b79daa64bf28c9ad
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-17 20:10:51 +0530

    ADDED : Test method to redirect security staff about parking is full

--- src/test/java/com/bridgelabz/parkinglotsystemtest/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/parkinglotsystemtest/ParkingLotSystemTest.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.parkinglotsystemtest;
 
+import com.bridgelabz.parkinglotsystem.AirportSecurity;
 import com.bridgelabz.parkinglotsystem.ParkingLotException;
+import com.bridgelabz.parkinglotsystem.ParkingLotOwner;
 import com.bridgelabz.parkinglotsystem.ParkingLotSystem;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,11 +11,14 @@ import org.junit.Test;
 public class ParkingLotSystemTest {
     ParkingLotSystem parkingLotSystem;
     Object vehicle;
+    ParkingLotOwner owner;
+    AirportSecurity airportSecurity = new AirportSecurity();
 
     @Before
     public void setUp() throws Exception {
         parkingLotSystem = new ParkingLotSystem(2);
         vehicle = new Object();
+        owner = new ParkingLotOwner();
     }
 
     @Test
@@ -54,7 +59,7 @@ public class ParkingLotSystemTest {
             boolean isVehicleParked = false;
             isVehicleParked = parkingLotSystem.isUnPark(new Object());
         } catch (ParkingLotException e) {
-            Assert.assertEquals(ParkingLotException.ExceptionType.VEHICLE_NOT_FOUND, e.type);
+            Assert.assertEquals(ParkingLotException.ExceptionType.PARKING_IS_FULL, e.type);
         }
     }
 
@@ -67,6 +72,34 @@ public class ParkingLotSystemTest {
             parkingLotSystem.isPark(new Object());
         } catch (ParkingLotException e) {
             Assert.assertEquals(ParkingLotException.ExceptionType.PARKING_IS_FULL, e.type);
+        }
+    }
+
+    @Test
+    public void givenParkingLotIsFull_ShouldInformOwner() {
+        boolean isParkingLotFull;
+        try {
+            parkingLotSystem.ParkingHandler(owner);
+            parkingLotSystem.isPark(vehicle);
+            parkingLotSystem.isPark(vehicle);
+            isParkingLotFull = owner.isParkingLotFull();
+            parkingLotSystem.isPark(vehicle);
+        } catch (ParkingLotException e) {
+            isParkingLotFull = owner.isParkingLotFull();
+            Assert.assertTrue(isParkingLotFull);
+        }
+    }
+
+    @Test
+    public void givenParkingLotIsFull_ShouldInformAirportSecurity() {
+        try {
+            parkingLotSystem.ParkingHandler(airportSecurity);
+            parkingLotSystem.isPark(vehicle);
+            parkingLotSystem.isPark(vehicle);
+            parkingLotSystem.isPark(vehicle);
+        } catch (ParkingLotException e) {
+            boolean isParkingLotFull = airportSecurity.isParkingLotFull();
+            Assert.assertTrue(isParkingLotFull);
         }
     }
 }
