973878251ae20e4efbf43d90e913eaa1d259f822
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-17 20:11:51 +0530

    REFACTOR : Code to achieve dry principle

--- src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglotsystem/ParkingLotSystem.java
@@ -1,17 +1,28 @@
 package com.bridgelabz.parkinglotsystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLotSystem {
     private int parkingLotCapacity;
     private int recentParkingLotSize;
     private Object vehicle;
+    List<ParkingLotHandler> parkingLotHandler;
 
     public ParkingLotSystem(int parkingLotCapacity) {
-        this.parkingLotCapacity = parkingLotCapacity;
+        parkingLotHandler = new ArrayList();
+    }
+
+    public void ParkingHandler(ParkingLotHandler owner) {
+        parkingLotHandler.add(owner);
     }
 
     public boolean isPark(Object vehicle) throws ParkingLotException {
-        if (this.recentParkingLotSize == this.parkingLotCapacity)
+        if (this.recentParkingLotSize == this.parkingLotCapacity) {
+            for (ParkingLotHandler handler : parkingLotHandler)
+                handler.parkingIsFull();
             throw new ParkingLotException(ParkingLotException.ExceptionType.PARKING_IS_FULL, "Parking Lot is full.");
+        }
         this.vehicle = vehicle;
         recentParkingLotSize++;
         return true;
